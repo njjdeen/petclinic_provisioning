bbefde52667ace9e197f0c8934dce484ac62ddd9
Niels Deen <niels.deen@devoteam.com>
2022-03-23 14:32:03 +0100

    modified JMeter and selenium test

--- JMeter-command-to-run.sh
+++ JMeter-command-to-run.sh
@@ -1,1 +1,1 @@
-$(Agent.HomeDirectory)/../../opt/apache-jmeter-5.3/bin/jmeter -n -f -t ./loadtest/petclinic_loadtest.jmx -e -o ./loadtest/dashboard
+$(Agent.HomeDirectory)/../../opt/apache-jmeter-5.3/bin/jmeter -n -f -t ./loadtest/petclinic_loadtest.jmx -Jhost=$(cat ~/testvm_public_IP ) -e -l ./loadtest/test_log -o ./loadtest/dashboard
